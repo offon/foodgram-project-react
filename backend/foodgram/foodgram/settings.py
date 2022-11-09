293bda905339b5a82e91320462f2a9668101f9c9
Николай Распопин <off_on@bk.ru>
2022-11-10 00:55:20 +0800

    setup docker-hub

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', '*')
 
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS'), 'web']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
