3ff2bbf832b2acb94567686745a9aef9b8a31f6d
Николай Распопин <off_on@bk.ru>
2022-11-10 23:11:27 +0800

    correct set_password_modul

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', '*')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS'), 'web']
 
